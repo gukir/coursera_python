a42a88703e5f32362df5f23f049b83c79d6c7b7e
KiryackenkoGU <GregoryKir@gmail.com>
2020-04-06 14:49:15 +0300

    8 week. Cockroach racing is done!!!

--- 8 week/cockroachracing.py
+++ 8 week/cockroachracing.py
@@ -2,37 +2,110 @@ from sys import stdin
 import itertools
 import operator
 import functools
+
 stdin = open('input.txt', 'r', encoding='utf8')
 # K - количество тараканов
 # N - количество болельщиков (ставок)
-# K, N = tuple(map(int, stdin.readline().split()))
-kn = map(int, stdin.readline().split())
-# Варианты итогов забегов
-variants = list(itertools.permutations(map(str, range(1, next(kn) + 1))))
-# Ставки
-bets = list(map(lambda tmp: stdin.readline().split(), range(next(kn))))
-# Отбор забегов, удовлетворящих условию "сыграла только одна из ставок" (XOR)
-good_variants = []
-good_variants2 = list(
-    filter(
-        lambda varics: all(
+# Ключи тестирования
+f_print = True
+f_data = True
+f_dbg = False
+
+
+# Проверка данных (поскольку итераторы перечисляются единожды
+if f_print:
+    # Чтение входных данных в итератор map, где 1 элемент - "permutations" (все возможные итоги забегов из K тараканов);
+    # 2 - входные данные из файла (по 2 ставки болельщиков)
+    vb = functools.reduce(
+        lambda K, N:
+        [itertools.permutations(
             map(
-                lambda y: operator.xor(varics.index(y[0]) < varics.index(y[1]),
-                                       varics.index(y[2]) < varics.index(y[3])),
-                bets
+                str, range(1, K + 1)
             )
-        ),
-        variants)
-)
-for variant in variants:
-    anybet = True
-    for bet in bets:
-        anybet &= operator.xor(variant.index(bet[0]) < variant.index(bet[1]),
-                               variant.index(bet[2]) < variant.index(bet[3]))
-    if anybet:
-        good_variants.append(variant)
-if good_variants:
-    print(*good_variants[0])
+        ), map(
+            lambda tmp:
+            stdin.readline().split(),
+            range(N)
+        )
+        ], map(int, stdin.readline().split())
+    )
+    # vb = map(
+    #     lambda kn:
+    #     list(
+    #         map(
+    #             lambda tmp:
+    #             stdin.readline().split(),
+    #             range(kn[1])
+    #         )
+    #     ) if kn[0]
+    #     else itertools.permutations(
+    #         map(str, range(1, kn[1] + 1))),
+    #     enumerate(
+    #         map(
+    #             int, stdin.readline().split()
+    #         )
+    #     )
+    # )
+    if f_data:
+        functools.reduce(
+            lambda v_test, b_test: print('Cписок вариантов и ставок:', *v_test, '', *b_test, sep='\n'), vb)
+    elif f_dbg:
+        v_test = next(vb)
+        b_test = next(vb)
+        res_test = list(filter(
+            lambda variant: all(
+                map(
+                    lambda bet: operator.xor(variant.index(bet[0]) < variant.index(bet[1]),
+                                             variant.index(bet[2]) < variant.index(bet[3])),
+                    b_test
+                )
+            ), v_test
+        ))
+    else:  # Комбинация двух предыдущих отлаженных if-ов
+        res_test = functools.reduce(
+            lambda v_test, b_test:
+            next(
+                filter(
+                    lambda variant:
+                    all(
+                        map(
+                            lambda bet:
+                            operator.xor(variant.index(bet[0]) < variant.index(bet[1]),
+                                         variant.index(bet[2]) < variant.index(bet[3])),
+                            b_test
+                        )
+                    ),
+                    v_test
+                ), 0
+            ),
+            vb
+        )
 else:
-    print(0)
+    print(*functools.reduce(
+        lambda v_test, b_test:
+        next(
+            filter(
+                lambda variant:
+                all(
+                    map(
+                        lambda bet:
+                        operator.xor(variant.index(bet[0]) < variant.index(bet[1]),
+                                     variant.index(bet[2]) < variant.index(bet[3])),
+                        b_test
+                    )
+                ),
+                v_test
+            ), [0]
+        ), functools.reduce(lambda k, n: [itertools.permutations(
+            map(
+                str, range(1, k + 1)
+            )
+        ), list(map(
+            lambda tmp:
+            stdin.readline().split(),
+            range(n)
+        ))
+        ], map(int, stdin.readline().split()))
+    ))
+
 eee = 0
--- 8 week/solution.py
+++ 8 week/solution.py
@@ -1,13 +1,30 @@
 from sys import stdin
-import math
-print(2,
-      *filter(
-        lambda el_in: all(
-          map(
-            lambda divider: el_in % divider != 0,
-            range(2, int(math.sqrt(el_in)) + 2)
-              )
-                          ),
-        range(3, int(stdin.readline()) + 1)
-              )
-      )
+import itertools
+import operator
+import functools
+print(*functools.reduce(
+        lambda v_test, b_test:
+        next(
+            filter(
+                lambda variant:
+                all(
+                    map(
+                        lambda bet:
+                        operator.xor(variant.index(bet[0]) < variant.index(bet[1]),
+                                     variant.index(bet[2]) < variant.index(bet[3])),
+                        b_test
+                    )
+                ),
+                v_test
+            ), [0]
+        ), functools.reduce(lambda k, n: [itertools.permutations(
+            map(
+                str, range(1, k + 1)
+            )
+        ), list(map(
+            lambda tmp:
+            stdin.readline().split(),
+            range(n)
+        ))
+        ], map(int, stdin.readline().split()))
+    ))
