f6612aef188d041aeb5a43cd3884d5e613852ca3
KiryackenkoGU <GregoryKir@gmail.com>
2020-04-07 13:04:20 +0300

    9 week. Matrix class is done!

---
+++ 9 week/solution.py
@@ -0,0 +1,34 @@
+from sys import stdin
+
+
+class Matrix:
+    def __init__(self, lists):
+        tmp_size = [0, 0]
+        self.data = []
+        for row in lists:
+            tmp_size[0] += 1
+            tmp_size[1] = 0
+            tmp_row = []
+            for elem in row:
+                tmp_row.append(elem)
+                tmp_size[1] += 1
+            self.data.append(tmp_row)
+        self.sz = tuple(tmp_size)
+
+    def __str__(self):
+        out_str = ''
+        for row in self.data:
+            row_str = ''
+            for elem in row:
+                row_str += (str(elem) + '\t')
+            row_str = row_str.strip()
+            row_str += '\n'
+            out_str += row_str
+        out_str = out_str.strip()
+        return out_str
+
+    def size(self):
+        return self.sz
+
+
+exec(stdin.read())
